876ddce7c46b598a4af9e3aa458a0db484af80cc
Juan Carlos <juancagb.17@gmail.com>
2015-12-08 20:46:57 -0500

    Avance de los modales de los combos

--- public/scripts/user/solicitar.js
+++ public/scripts/user/solicitar.js
@@ -3,6 +3,7 @@ $(document).on('ready', funcPrincipal);
 function funcPrincipal() {
     // Al hacer click en un plato, se permite mostrar sus detalles
     $('.img-thumbnail').next().on('click', mostrarDetalles);
+    $('.combo').next().on('click', mostrarDetallesCombo);
     $('[data-combo]').on('click', enviarDatos);
 }
 
@@ -10,6 +11,15 @@ function mostrarDetalles() {
     // Obtener el id del plato del data-id
     var idPlato = $(this).prev().data('id');
     var selector = '.modal[data-plato='+idPlato+']';
+    var $modalDetalles = $(selector);
+    // Mostrar modal
+    $modalDetalles.modal('show');
+}
+
+function mostrarDetallesCombo() {
+    // Obtener el id del plato del data-id
+    var idComboPlato = $(this).data('comboplato');
+    var selector = '.modal[data-comboplatoId='+idComboPlato+']';
     var $modalDetalles = $(selector);
     // Mostrar modal
     $modalDetalles.modal('show');
